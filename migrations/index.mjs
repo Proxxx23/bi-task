7b4cf13cec28d357f0ddeea98253d1438e7d960d
Łukasz Matusik <lukasz.matusik@schibsted.com>
2022-09-02 15:38:21 +0200

    Migration command

--- migrations/index.mjs
+++ migrations/index.mjs
@@ -1,8 +1,9 @@
 import fs from "fs";
 import {resolve} from "path";
+import {exec} from "child_process";
 
 (() => {
   const files = fs.readdirSync(resolve('./migrations/sqlite'));
 
-  files.forEach((file) => fs.openSync(resolve('./migrations/sqlite/' + file)));
+  files.forEach((file) => exec('node ' + resolve('./migrations/sqlite/' + file)));
 })();
